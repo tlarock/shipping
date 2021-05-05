17dfa8b90d331cde58eb2c6038774864daa1a658
Tim LaRock <timothylarock@gmail.com>
2021-05-05 11:16:06 -0400

    Updated heapq priority queue docstring.

--- code/heappq.py
+++ code/heappq.py
@@ -1,6 +1,11 @@
 import itertools
 from heapq import heappush, heappop
 class HeapPQ():
+    '''
+    Priority queue implementation using _heapq_. Very slight modification
+    of the implementation in the documentation for heapq located at:i
+        https://docs.python.org/3.6/library/heapq.html#priority-queue-implementation-notes
+    '''
     def __init__(self):
         self.pq = []                         # list of entries arranged in a heap
         self.entry_finder = {}               # mapping of tasks to entries
@@ -22,7 +27,7 @@ class HeapPQ():
         entry[-1] = self.REMOVED
 
     def pop_task(self):
-        'Remove and return the lowest priority task. Raise KeyError if empty.'
+        'Remove and return the lowest priority task. Return False if empty.'
         while self.pq:
             priority, count, task = heappop(self.pq)
             if task is not self.REMOVED:
