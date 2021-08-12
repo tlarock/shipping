c1eb284d1abeb3563195e27859ff545337761052
Tim LaRock <timothylarock@gmail.com>
2021-08-12 13:57:40 -0400

    Added coroute graph to intersect_paths.

--- code/intersect_paths.py
+++ code/intersect_paths.py
@@ -30,6 +30,10 @@ gwdata = pd.read_csv('../data/original/Nodes_2015_country_degree_bc_B_Z_P.csv', 
 port_to_id = dict(zip(gwdata.port.values,gwdata.id.values))
 id_to_port = dict(zip(gwdata.id.values,gwdata.port.values))
 
+print("Reading coroute graph paths.", flush=True)
+with open('../results/interpolated_paths/shortest_paths_coroute.pickle', 'rb') as fpickle:
+    cr_paths = pickle.load(fpickle)
+
 
 print("Reading path graph paths.", flush=True)
 with open('../results/interpolated_paths/shortest_paths_pathrep.pickle', 'rb') as fpickle:
@@ -41,12 +45,10 @@ with open('../results/interpolated_paths/shortest_paths_pathrep.pickle', 'rb') a
 routes = []
 G_indirect = nx.DiGraph()
 G = nx.DiGraph()
-edge_indices = dict()
 with open('../data/all_routes_2015.ngram', 'r') as fin:
     for route_id, line in enumerate(fin, start=1):
         route = remove_selfloops(line.strip().split(','))
         routes.append(route)
-        edge_indices[route_id] = dict()
         for i in range(1, len(route)):
             if G.has_edge(route[i-1], route[i]):
                 G[route[i-1]][route[i]]['routes'].add(route_id)
@@ -54,8 +56,7 @@ with open('../data/all_routes_2015.ngram', 'r') as fin:
                 G.add_edge(route[i-1], route[i], routes=set([route_id]))
 
             edge = (route[i-1], route[i])
-            edge_indices[route_id].setdefault(edge, set())
-            edge_indices[route_id][edge].add(i-1)
+
         if route[0] != route[-1]:
             for i in range(len(route)):
                 for j in range(i+1, len(route)):
@@ -76,9 +77,10 @@ with open('/scratch/larock.t/shipping/results/interpolated_paths/iterative_paths
     ## These two dictionaries are the output
     minroutes_cg = dict()
     minroutes_pg = dict()
+    minroutes_cr = dict()
     pair_counter = 0
     total_pairs = 0
-    prev_pair = (-1,-1)
+    prev_pair = (-1, -1)
     first = True
     for line in fin:
         path, mr_dist, route_dist, *_ = line.strip().split('|')
@@ -94,14 +96,7 @@ with open('/scratch/larock.t/shipping/results/interpolated_paths/iterative_paths
             ## CLIQUE GRAPH PATHS
             for path in paper_paths_dict[mpair]:
                 mpath = [id_to_port[path[i]] for i in range(len(path))]
-                ## If the nodes have a direct connection in the clique
-                ## graph, they are part of the same route and so we say
-                if len(mpath) == 2 and tuple(mpath) in directed_clique_edges:
-                    tpath = tuple(mpath)
-                    minroutes_cg.setdefault(prev_pair, dict())
-                    minroutes_cg[prev_pair][tpath] = 1
-
-                ## Ignore shortest paths through the clique graph 
+                ## Ignore shortest paths through the clique graph
                 ## that are not at all possible in the path based graph
                 if not all([(mpath[i-1], mpath[i]) in G.edges() for i in range(1, len(mpath))]):
                     continue
@@ -121,6 +116,30 @@ with open('/scratch/larock.t/shipping/results/interpolated_paths/iterative_paths
                     route_lengths.append(number_of_routes)
                     minroutes_cg.setdefault(prev_pair, dict())
                     minroutes_cg[prev_pair][tpath] = number_of_routes
+
+            ## COROUTE GRAPH PATHS
+            for path in cr_paths[prev_pair]:
+                ## Ignore shortest paths through the coroute graph
+                ## that are not at all possible in the path based graph
+                if not all([(path[i-1], path[i]) in G.edges() for i in range(1, len(path))]):
+                    continue
+
+                ## Check if the path is minimum-route by checking whether it appears
+                ## in the unfiltered minimum-route paths
+                tpath = tuple(path)
+                if tpath in minroute_paths[prev_pair]:
+                    minroutes_cr.setdefault(prev_pair, dict())
+                    minroutes_cr[prev_pair][tpath] = int(minroute_paths[prev_pair][tpath])
+                else:
+                    noi = set(path)
+                    subgraph = nx.DiGraph()
+                    for i in range(1, len(path)):
+                        subgraph.add_edge(path[i-1], path[i], routes=G[path[i-1]][path[i]]['routes'])
+
+                    number_of_routes = greedy_number_of_routes(path, subgraph, routes)
+                    minroutes_cr.setdefault(prev_pair, dict())
+                    minroutes_cr[prev_pair][tpath] = number_of_routes
+
 
             ## PATH GRAPH PATHS
             for path in pg_paths[prev_pair]:
@@ -160,14 +179,7 @@ mpair = (port_to_id[prev_pair[0]], port_to_id[prev_pair[1]])
 ## CLIQUE GRAPH PATHS
 for path in paper_paths_dict[mpair]:
     mpath = [id_to_port[path[i]] for i in range(len(path))]
-    ## If the nodes have a direct connection in the clique
-    ## graph, they are part of the same route and so we say
-    if len(mpath) == 2 and tuple(mpath) in directed_clique_edges:
-        tpath = tuple(mpath)
-        minroutes_cg.setdefault(prev_pair, dict())
-        minroutes_cg[prev_pair][tpath] = 1
-
-    ## Ignore shortest paths through the clique graph 
+    ## Ignore shortest paths through the clique graph
     ## that are not at all possible in the path based graph
     if not all([(mpath[i-1], mpath[i]) in G.edges() for i in range(1, len(mpath))]):
         continue
@@ -187,6 +199,29 @@ for path in paper_paths_dict[mpair]:
         route_lengths.append(number_of_routes)
         minroutes_cg.setdefault(prev_pair, dict())
         minroutes_cg[prev_pair][tpath] = number_of_routes
+
+## COROUTE GRAPH PATHS
+for path in cr_paths[prev_pair]:
+    ## Ignore shortest paths through the coroute graph
+    ## that are not at all possible in the path based graph
+    if not all([(path[i-1], path[i]) in G.edges() for i in range(1, len(path))]):
+        continue
+
+    ## Check if the path is minimum-route by checking whether it appears
+    ## in the unfiltered minimum-route paths
+    tpath = tuple(path)
+    if tpath in minroute_paths[prev_pair]:
+        minroutes_cr.setdefault(prev_pair, dict())
+        minroutes_cr[prev_pair][tpath] = int(minroute_paths[prev_pair][tpath])
+    else:
+        noi = set(path)
+        subgraph = nx.DiGraph()
+        for i in range(1, len(path)):
+            subgraph.add_edge(path[i-1], path[i], routes=G[path[i-1]][path[i]]['routes'])
+
+        number_of_routes = greedy_number_of_routes(path, subgraph, routes)
+        minroutes_cr.setdefault(prev_pair, dict())
+        minroutes_cr[prev_pair][tpath] = number_of_routes
 
 ## PATH GRAPH PATHS
 for path in pg_paths[prev_pair]:
@@ -211,3 +246,6 @@ with open('/scratch/larock.t/shipping/results/interpolated_paths/clique_minroute
 
 with open('/scratch/larock.t/shipping/results/interpolated_paths/sp_pathrep_minroutes.pickle', 'wb') as fpickle:
     pickle.dump(minroutes_pg, fpickle)
+
+with open('/scratch/larock.t/shipping/results/interpolated_paths/coroute_minroute_paths.pickle', 'wb') as fpickle:
+    pickle.dump(minroutes_cr, fpickle)
