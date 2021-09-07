8b3abd52738844185c635d269b5f51c2abec09bb
tlarock <larock.t@husky.neu.edu>
2021-09-07 09:27:55 -0400

    Minor changes to rank_routes.

--- code/rank_routes.py
+++ code/rank_routes.py
@@ -4,13 +4,14 @@ from iterative_minroutes import write_filtered
 args = sys.argv
 
 redundancy_thresh = 1.0
-#scratch_base = '/scratch/larock.t/shipping/results/interpolated_paths/'
-scratch_base = '../results/interpolated_paths/'
+scratch_base = '/scratch/larock.t/shipping/results/interpolated_paths/'
+#scratch_base = '../results/interpolated_paths/'
 with open(scratch_base + 'iterative_paths.txt', 'r') as fin:
-    route_counts = {'weighted':dict(), 'counts':dict()}
+    route_counts = {'weighted':dict(), 'unweighted':dict()}
+    pair = (-1,-1)
+    prev_pair = None
     pair_counter = 0
     total_pairs = 0
-    first = True
     for line in fin:
         path, mr_dist, route_dist, *routes = line.strip().split('|')
         list_routes = []
@@ -19,19 +20,22 @@ with open(scratch_base + 'iterative_paths.txt', 'r') as fin:
             for rt_str in route.split(','):
                 try:
                     rt, num = rt_str.split(':')
-                    route_counts['counts'].setdefault(rt, 0)
+                    route_counts['unweighted'].setdefault(rt, 0)
                     route_counts['weighted'].setdefault(rt, 0)
-                    route_counts['counts'][rt] += 1
+                    route_counts['unweighted'][rt] += 1
                     route_counts['weighted'][rt] += int(num)
                 except Exception as e:
-                    print(f'{rt_str} was not succesfully parsed.')
+                    print(f'{rt_str} was not succesfully parsed.', flush=True)
                     continue
 
-        pair_counter += 1
-        total_pairs += 1
-        if pair_counter == 50_000:
-            print(total_pairs)
-            pair_counter = 0
+        pair = path[0], path[-1]
+        if pair != prev_pair:
+            pair_counter += 1
+            total_pairs += 1
+            if pair_counter == 50_000:
+                print(total_pairs, flush=True)
+                pair_counter = 0
+        prev_pair = pair
 
 import pickle
 with open('../results/route_counts.pickle', 'wb') as fpickle:
